a1eac0479c65a9da57d4f7c041845789d740ac0a
fangkuaichaoge <2963329567@qq.com>
2026-03-15 14:05:13 +0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -27,6 +27,7 @@
 #define LOG_TAG "RenderFusion"
 #define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
 #define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
+#define LOGW(...) __android_log_print(ANDROID_LOG_WARN, LOG_TAG, __VA_ARGS__)
 
 // ===================== GL ERROR CHECK MACRO =====================
 #define CHECK_GL_ERROR() { \
